1b5b723bcd9482571be6b27cc94725a0a384ca76
saikian888 <saikiranmandula1998@gmail.com>
2024-10-20 15:19:24 -0400

    Simple Rnn project

---
+++ app.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+
+word_index=imdb.get_word_index()
+
+reverse_word_index ={value:key for key,value in word_index.items()}
+reverse_word_index
+
+model=load_model('simple_rnn_imdb.h5')
+
+# Step 2: Helper Functions
+# Function to decode reviews
+def decode_review(encoded_review):
+    return ' '.join([reverse_word_index.get(i - 3, '?') for i in encoded_review])
+
+# Function to preprocess user input
+def preprocess_text(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word, 2) + 3 for word in words]
+    padded_review = sequence.pad_sequences([encoded_review], maxlen=500)
+    return padded_review
+
+### Prediction  function
+
+def predict_sentiment(review):
+    preprocessed_input=preprocess_text(review)
+
+    prediction=model.predict(preprocessed_input)
+
+    sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+    
+    return sentiment, prediction[0][0]
+
+## streamlit app
+import streamlit as st
+st.title("IMDB Movie Review Sentiment Analysis App")
+st.write('Enter a movie review to classify it as positive or negative.')
+
+review = st.text_input("Movie Review")
+if st.button('Classify'):
+    preprocess_input=preprocess_text(review)
+    prediction= model.predict(preprocess_input)
+    sentiment='Positive' if prediction[0][0]>0.5 else 'Negative'
+    st.write(f'Sentiment: {sentiment}')
+    st.write(f'Prediction Score: {prediction[0][0]}')
+else:
+    st.write('Please enter a review and click the button to classify it.')
